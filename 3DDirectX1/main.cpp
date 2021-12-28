7c34f0efaf99b4b5643b6caa23f0bbcfd7db91d1
Nakata_Moeka <k020g1155@g.neec.ac.jp>
2021-12-28 14:59:44 +0900

    mainの整理

--- 3DDirectX1/main.cpp
+++ 3DDirectX1/main.cpp
@@ -1,69 +1,65 @@
-﻿
-#include"input.h"
-#include"WinApp.h"
-#include"Sprite.h"
+﻿#include"WinApp.h"
 #include"Audio.h"
 #include"DirectXCommon.h"
-#include"DebugText.h"
-#include"Object3d.h"
-#include"Model.h"
-
 #include "GameScene.h"
 
-#pragma region 初期化
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	HRESULT result;
-	//ウィンドウの生成
+	
+
 	WinApp* winapp = nullptr;
+	DirectXCommon* dxcommon = nullptr;
+	Audio* audio = nullptr;
+	Input* input = nullptr;
+	GameScene* gameScene = nullptr;
+
+	//ウィンドウの生成
 	winapp = new WinApp();
 	winapp->CreateGameWindow();
-	DirectXCommon* dxcommon = nullptr;
+	//DirectXの初期化
 	dxcommon = new DirectXCommon();
 	dxcommon->Initialize(winapp);
 	
-	Audio* audio = nullptr;
+	//汎用機能の初期化
 	audio = new Audio();
 	if (!audio->Initialize()) {
 		assert(0);
 		return 1;
 	}
 
-	Sprite* sprite = nullptr;
-
 	if (!Sprite::StaticInitialize(dxcommon->Getdev(), WinApp::window_width, WinApp::window_height)) {
 		assert(0);
 		return 1;
 	}
 
-	// 3Dオブジェクト静的初期化
 	if (!Object3d::StaticInitialize(dxcommon->Getdev(), WinApp::window_width, WinApp::window_height)) {
 		assert(0);
 		return 1;
 	}
-
-	Input* input = nullptr;
+	
 	input = new Input();
 	input->Initialize(winapp);
 
-	GameScene* gameScene = nullptr;
+	//ゲームシーンの初期化
 	gameScene = new GameScene();
 	gameScene->Initialize(dxcommon, input, audio);
 
-while (true) {
-if (winapp->ProcessMessage()) { break; }
-
-
-		input->update();
-		
-		gameScene->Update();
-
-		dxcommon->preDraw();
-		gameScene->Draw();
-		dxcommon->postDraw();
-		// DirectX毎フレーム処理　ここまで
+//ループ
+	while (true) 
+	{
+		if (winapp->ProcessMessage()) { break; }
+			//入力関連の毎フレーム処理
+			input->update();
+			//ゲームシーンの毎フレーム処理
+			gameScene->Update();
+			//描画
+			dxcommon->preDraw();
+			gameScene->Draw();
+			dxcommon->postDraw();
 	}
 	 //ウィンドウクラスを登録解除
 	winapp->TerminateGameWindow();
+	
 	// 各種解放
 	safe_delete(gameScene);
 	safe_delete(audio);
